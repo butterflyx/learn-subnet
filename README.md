d9e2918e91550be6cf303845bcb44749b0723913
Jean-Christoph von Oertzen <5388631+butterflyx@users.noreply.github.com>
2020-07-17 18:30:20 -0400

    added ipcalc lib

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # learn-subnet
 
-simple bash script to learn subnetting with random IPs
+bash script to learn subnetting with random IPs
 
 
 ## Install
@@ -18,3 +18,25 @@ $ cd learn-subnet/
 
 ### use it
 ![learn-subnet](https://user-images.githubusercontent.com/5388631/87831400-69bb2500-c851-11ea-85ac-46425b46f5f1.png)
+
+
+## offline results
+
+this package includes the perl-script "ipcalc" from Krischan Jodies in the lib folder.
+Use the `-t [time]` parameter to set time until solutions will be displayed.
+
+You can use the time values as for the Unix `sleep` command:
+- s for seconds (can be omited)
+- m for minutes.
+- h for hours.
+- d for days.
+
+### example for 1 minute wait time
+![learn-subnet_solution](https://user-images.githubusercontent.com/5388631/87835260-a2abc780-c85a-11ea-92d9-e198f3cdfbc1.png)
+
+
+## credits
+
+### IPv4 Calculator
+Copyright (C) Krischan Jodies 2000 - 2005
+krischan()jodies.de, http://jodies.de/ipcalc
--- learnip.sh
+++ learnip.sh
@@ -15,6 +15,17 @@ BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
 
+while getopts "t:" opt; do
+  case $opt in
+	t) Timer="$OPTARG"
+	;;
+    \?) echo -e "${RED}[-]${NC} Invalid option -$OPTARG"
+		exit 1
+    ;;
+  esac
+done
+
+
 declare -a tasks=("${YELLOW}network address${NC} and ${YELLOW}broadcast address${NC}" "${YELLOW}network class${NC} and ${YELLOW}maximum hosts${NC}" "${YELLOW}first usable IP${NC} and ${YELLOW}last usable IP${NC}")
 num_tasks=${#tasks[*]}
 
@@ -24,7 +35,11 @@ task=${tasks[$((RANDOM%num_tasks))]}
 myip="`shuf -i 1-233 -n 1`.`shuf -i 0-255 -n 1`.`shuf -i 0-255 -n 1`.`shuf -i 0-255 -n 1`";
 subnet="`shuf -i 8-32 -n 1`";
 
-link="http://jodies.de/ipcalc?host=${myip}&mask1=${subnet}&mask2=";
+if [ ! -f ./lib/ipcalc ]; then
+    Timer=""
+	link="http://jodies.de/ipcalc?host=${myip}&mask1=${subnet}&mask2=";
+fi
+
 
 echo ""
 echo -e "${GREEN}
@@ -45,5 +60,13 @@ echo "----------------------------"
 echo -e "     ${GREEN}${myip}/${subnet}${NC}"
 echo "----------------------------"
 echo ""
-echo -e "for solution see ${BLUE}${link}${NC} (Strg+click)"
+
+if [[ ! -z $Timer ]];then
+	echo -e "time until solution will be displayed: ${Timer} ............"
+	sleep $Timer
+	./lib/ipcalc ${myip}/${subnet}
+else
+	echo -e "for solution see ${BLUE}${link}${NC} (Strg+click)"
+fi
+
 echo ""
